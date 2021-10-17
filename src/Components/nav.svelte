6a7b44f51b65414928d88ddbda3280fa58dcb526
vdtlv <komapcheg@gmail.com>
2021-10-17 20:56:10 +0300

    new colors added

--- src/Components/nav.svelte
+++ src/Components/nav.svelte
@@ -1,6 +1,22 @@
 <!-- svelte-ignore missing-declaration -->
 <script lang="ts">
   import { page } from "./stores.js";
+
+  function toggle() {
+    if ($page == 0 || $page == 2) {
+      window.document.body.classList.remove("about-mode", "work-mode");
+    }
+
+    if ($page == 1) {
+      window.document.body.classList.remove("work-mode");
+      window.document.body.classList.add("about-mode");
+    }
+
+    if ($page == 3) {
+      window.document.body.classList.remove("about-mode");
+      window.document.body.classList.add("work-mode");
+    }
+  }
 </script>
 
 <div class="container">
@@ -17,18 +33,54 @@
           ? "projects"
           : "work"}
       >
-        <li on:click={page.main} class={$page == 0 ? "active" : ""}>●</li>
-        <li on:click={page.about} class={$page === 1 ? "active" : ""}>About</li>
-        <li on:click={page.projects} class={$page === 2 ? "active" : ""}>
+        <li
+          on:click={page.main}
+          on:click={toggle}
+          class={$page == 0 ? "active" : ""}
+        >
+          ●
+        </li>
+        <li
+          on:click={page.about}
+          on:click={toggle}
+          class={$page === 1 ? "active" : ""}
+        >
+          About
+        </li>
+        <li
+          on:click={page.projects}
+          on:click={toggle}
+          class={$page === 2 ? "active" : ""}
+        >
           Projects
         </li>
-        <li on:click={page.work} class={$page === 3 ? "active" : ""}>Work</li>
+        <li
+          on:click={page.work}
+          on:click={toggle}
+          class={$page === 3 ? "active" : ""}
+        >
+          Work
+        </li>
       </ul>
     </div>
   </div>
 </div>
 
 <style>
+  :global(body.about-mode) {
+    background-color: #ffd80d;
+    color: #222;
+    transition-delay: 0.2s;
+    transition: background-color 0.2s;
+  }
+
+  :global(body.work-mode) {
+    background-color: #1544bb;
+    color: #fff;
+    transition-delay: 0.2s;
+    transition: background-color 0.2s;
+  }
+
   .container {
     height: 180px;
     display: flex;
@@ -39,13 +91,12 @@
   }
   .item {
     align-self: center;
-    color: #222222;
+
     font-weight: bold;
     font-size: 16px;
     line-height: 19px;
   }
   .title {
-    color: #222222;
     font-weight: bold;
     font-size: 16px;
     line-height: 19px;
@@ -66,24 +117,46 @@
     padding-left: 0;
     color: #bababa;
     transition-property: padding-top, padding-bottom;
-    transition-duration: 0.5s;
+    transition-duration: 0.2s;
+  }
+  :global(body.about-mode) ul {
+    color: #fff;
+  }
+  :global(body.work-mode) ul {
+    color: #a8bef5;
   }
 
-  li:hover,
-  li:active {
+  li:hover {
     width: min-content;
     background-color: #fbfbfb;
     transition: 0.2s;
+    transition: 0.2s;
+    cursor: pointer;
+  }
+
+  :global(body.about-mode) li:hover {
+    background-color: rgba(255, 255, 255, 0.1);
+  }
+  :global(body.work-mode) li:hover {
+    background-color: rgba(255, 255, 255, 0.1);
+  }
+
+  .main > .active {
+    color: #ffd80d;
   }
 
   li {
     border-radius: 4px;
     padding: 0 8px 0 8px;
-    cursor: pointer;
   }
   .active {
-    color: #222222;
+    color: #222;
+    align-items: baseline;
   }
+  :global(body.work-mode) .active {
+    color: #fff;
+  }
+
   .main {
     padding-top: 76px;
   }
--- src/Components/work.svelte
+++ src/Components/work.svelte
@@ -4,7 +4,7 @@
 
 <div
   class="container"
-  in:fly={{ y: 50, duration: 600, delay: 400 }}
+  in:fly={{ y: 20, duration: 200, delay: 200 }}
   out:fade={{ duration: 200 }}
 >
   <p>Everything is fine for now</p>
