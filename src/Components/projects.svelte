6a0e9e1bf62df72225cc53f617292e2d1db6e3ce
Viacheslav Diatlov <vdmacmini@Slavas-Mac-mini.local>
2022-02-02 00:33:19 +0300

    projects downgrade

--- src/Components/projects.svelte
+++ src/Components/projects.svelte
@@ -7,68 +7,54 @@
   in:fly={{ y: 20, duration: 200, delay: 200 }}
   out:fade={{ duration: 200 }}
 >
-  <div class="project">
-    <img width="" alt="pr1" />
-    <h6>Project 1 →</h6>
-  </div>
-  <div class="project">
-    <img width="" alt="pr2" />
-    <h6>Project 2</h6>
-  </div>
-  <div class="project">
-    <img width="" alt="pr3" />
-    <h6>Project 3</h6>
-  </div>
-  <div class="project">
-    <img width="" alt="pr4" />
-    <h6>Project 4</h6>
-  </div>
+<h6> this page will be updated soon, but you still can
+<a href="https://vdtlv.notion.site/6c50f368362342208872db178dfb696f">check my projects at notion</a>
+</h6>
 </div>
 
 <style>
   .main {
     display: flex;
-    flex-direction: row;
-    flex-wrap: wrap;
-    justify-content: space-evenly;
-    align-content: stretch;
-    gap: 16px;
+    justify-content: center;
+    align-items: flex-end;
+    height: calc(100vh - 248px);
+    color: #4d4d4d;
+    margin: 0px 16px;
+    
+  }
+  
+  a {
     color: var(--text-color);
-    column-gap: 24px;
-    padding: 1em;
-    margin: 0 auto;
-  }
-
-  img {
-    width: 90vw;
-    height: auto;
-  }
-
-  h6 {
-    margin-top: 0.5em;
-    font-size: 1.3em;
-  }
-
-  h6:hover {
     text-decoration: underline;
   }
 
+  h6 {
+    margin: 0px;
+    font-size: 3em;
+  }
+
   @media (min-width: 920px) {
-    .main {
-      display: flex;
-      flex-direction: row;
-      flex-wrap: wrap;
-      justify-content: space-evenly;
-      align-content: stretch;
-      gap: 16px;
-    }
-    img {
-      width: 40vw;
+    .main{
+      align-items: center;
+      text-align: center;
     }
     h6 {
-      width: fit-content;
-      height: auto;
-      color: var(--text-color);
+      font-size: 5em;
+    }
+  }
+  @media (min-width: 1395px) {
+    .main{
+      margin: auto;
+      max-width: 80%;
+    }
+    h6 {
+      font-size: 7em;
+    }
+
+  }
+  @media (max-width: 320px) {
+    h6 {
+      margin: 0px;
       font-size: 2em;
     }
   }
