364aaa65ef7b3701c7e897f4f6be155c9d90b184
vdtlv <vdmacmini@Slavas-Mac-mini.local>
2022-02-06 06:02:35 +0300

    99% lol

--- src/Components/mainpage.svelte
+++ src/Components/mainpage.svelte
@@ -4,23 +4,66 @@
 
 <div
   class="main"
+  style="height: calc(100% - 212px);"
   in:fly={{ y: 20, duration: 200, delay: 200 }}
   out:fade={{ duration: 200 }}
 >
+  <div>
+    <p class="f">I am collecting services, can help in design process.</p>
+    <a class="button" href="https://vdtlv.notion.site"> List of links → </a>
+  </div>
+
+  <div class="sub">
   <h1>Hi, I am Slava</h1>
-  <p>And I am updating this website as fast as it's possible</p>
-  <p>🤡🤡🤡</p>
+  <p >And I am updating this website<br>as fast as it's possible <br><br> 🤡🤡🤡</p>
+  </div>
+  
 </div>
 
 <style>
+
+  .sub {
+    display: block;
+    text-align: center;
+  }
+  .button {
+    display: block;
+    background-color: var(--hover);
+    border: none;
+    border-radius: 4px;
+    padding: 14px 16px;
+    margin: 8px 0px;
+    font-weight: bold;
+    font-size: 1em;
+    color: var(--text-color);
+    transition: background-color 0.3s;
+    text-align: center;
+  }
+  .button:hover{
+      text-decoration: none;
+      background-color: #e9e9e9;
+      transition: background-color 0.3s;
+    }
+  
   .main {
-    padding: 1em;
+    padding: 0px 1em;
     margin: 0 auto;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
   }
 
+
   @media (min-width: 640px) {
+    .button { 
+      width: fit-content;
+    }
     .main {
       max-width: 768px;
     }
+    .f {
+    display: block;
+    width: 200px;
+  }
   }
 </style>
--- src/Components/work.svelte
+++ src/Components/work.svelte
@@ -25,7 +25,7 @@
   <h2 class="{hoverlink != 0 ? 'hvr' : ''}">UI/UX Designer at <a class="{hoverlink == 1 ? 'hvr' : ''}" on:mouseover={handleMouseOver2} on:mouseout={handleMouseOut} href="https://rightontrek.com">RightOnTrek</a> since September 2021</h2>
   <!-- svelte-ignore a11y-mouse-events-have-key-events -->
   <h2 class="{hoverlink != 0 ? 'hvr' : ''}">Started <a class="{hoverlink == 2 ? 'hvr' : ''}" on:mouseover={handleMouseOver} on:mouseout={handleMouseOut}
-              href="https://educt.ru"> Educt</a> with my folks</h2>
+              href="https://educt.ru"> Educt</a> with&nbsp;my&nbsp;folks</h2>
   <h2 class="{hoverlink != 0 ? 'hvr' : ''}">Graduated from ITMO&nbsp;University in 2021</h2>
   <h2 class="{hoverlink != 0 ? 'hvr' : ''}">First experience in startup with a good team</h2>
   <h2 class="{hoverlink != 0 ? 'hvr' : ''}">First part-time job at Hypefactory in 2019</h2>
@@ -44,11 +44,14 @@
     color: var(--text-color);
     padding: 1em;
     margin: 0 auto;
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    max-width: 100%;
   }
 
   h2 {
     font-size: 2rem;
-    margin-bottom: 3rem;
     transition: color 0.3s;
   }
 
@@ -66,9 +69,37 @@
     text-decoration: none;
   }
 
+  @media (max-width: 1000px) {
+    .hvr {
+      color: var(--white);
+    }
+    a {
+    color: var(--default);
+  }
+  a.hvr {
+    color: var(--default);
+  }
+  }
+
+
   @media (min-width: 640px) {
     .main {
       max-width: 768px;
+      gap: 1em 84px;
+    }
+    .hvr {
+      color: var(--hover);
+      transition: color 0.3s;
+    }
+    h2 {
+      max-width: 320px;
+    }
+    a {
+      color: var(--text-color);
+    }
+    a:hover {
+      transition: color 0.3s;
+     text-decoration: none;
     }
   }
 </style>
