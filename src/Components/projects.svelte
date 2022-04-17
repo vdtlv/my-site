dd7ea502e1803a9fa24644d50ff243419e8c71ba
vdtlv <komapcheg@gmail.com>
2022-04-17 23:25:18 +0300

    projects update

--- src/Components/projects.svelte
+++ src/Components/projects.svelte
@@ -2,7 +2,7 @@
   import { fade, fly } from "svelte/transition";
 </script>
 
-<div
+<!-- <div
   class="main"
   style="height: calc(100% - 212px);"
   in:fly={{ y: 20, duration: 200, delay: 200 }}
@@ -11,10 +11,66 @@
 <h6> this page will be updated soon, but you still can
 <a href="https://vdtlv.notion.site/6c50f368362342208872db178dfb696f">check my projects at notion</a>
 </h6>
+</div> -->
+
+<div class="projectlist" in:fly={{ y: 20, duration: 200, delay: 200 }}
+out:fade={{ duration: 200 }}>
+<a href="https://vdtlv.notion.site/Educt-co-founder-1800b5fd8ead4e17a6f37a7b2470e69d" target="_blank" class="project" >
+  <img src="Images/educt.png" alt="cool animation representing main feature of epost">
+  <h6>Educt</h6>
+</a>
+
+<a href="https://vdtlv.notion.site/Phase-10-student-project-5584573a3177445ba2b402a60f0d6f99" target="_blank" class="project" >
+  <img src="Images/phase.png" alt="cool animation representing main feature of epost">
+  <h6 >Phase 10</h6>
+</a>
+  
+
+
+  <a href="https://vdtlv.notion.site/ePost-Diploma-project-22b01964f80c424ca83e8d097d0ffdb6" target="_blank" class="project" >
+    <img src="Images/epost.gif" alt="cool animation representing main feature of epost">
+    <h6 >ePost</h6>
+  </a>
+
 </div>
 
 <style>
-  .main {
+  img {
+    max-width: 100%;
+    max-height: 100%;
+    margin-bottom: 8px;
+    border-radius: 8px;
+    transition: 0.2s;
+  }
+
+  .projectlist {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    margin: 0 16px;
+  }
+
+  .project {
+    display: block;
+    height: auto; 
+    width: 30%; 
+    /* background-color: rgb(103, 207, 103); */ 
+    justify-content: center;
+    align-items: center;
+    margin: 0 16px 24px 16px;
+    text-align: center;
+  }
+  .project:hover > img {
+    transition: 0.28s;
+    transform: scale(1.05);
+    box-shadow: 0px -8px 24px #1b1b1b;
+  }
+  .project:hover > h6 {
+    color: var(--text-color);
+    transition: 0.28s;
+  }
+  /* 
+      .main {
     display: flex;
     justify-content: center;
     align-items: flex-end;
@@ -22,40 +78,62 @@
     margin: 0px 16px;
     height: -webkit-fill-available;
   }
-  
-  a {
-    color: var(--text-color);
-    text-decoration: underline;
+  .projectlist::after {
+    content: "";
+    flex: auto;
+  } */
+
+
+
+  @media (max-width: 1008px) {
+    .project { width: 45%; }
   }
 
+  @media (max-width: 695px) {
+    .project { 
+      width: 100%;
+      margin: 0 0 24px 0;
+    }
+    
+  }
+  
   h6 {
-    margin: 0px;
-    font-size: 3em;
+    margin: 8px 0px 0px;
+    color: var(--default);
+    font-size: 1.2em;
+    font-weight: 600;
+    display: inline-block;
+    transition: 0.28s;
+  }
+  h6:hover {
+    text-decoration: none;
+    transition: 0.2s;
+    cursor: pointer;
   }
 
   @media (min-width: 920px) {
-    .main{
+    /*.main{
       align-items: center;
       text-align: center;
     }
     h6 {
       font-size: 5em;
-    }
+    }*/
   }
   @media (min-width: 1395px) {
-    .main{
+    /*.main{
       margin: auto;
       max-width: 80%;
     }
     h6 {
       font-size: 7em;
-    }
+    }*/
 
   }
   @media (max-width: 320px) {
-    h6 {
+    /*h6 {
       margin: 0px;
       font-size: 2em;
-    }
+    }*/
   }
 </style>
